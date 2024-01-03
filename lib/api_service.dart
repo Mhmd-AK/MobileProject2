510ed04ec7aa3538eb4c2b6d15ab9febe8fda1ac
souadkhalifeh <souadkhalifeh620@gmail.com>
2024-01-03 10:38:04 +0200

    edited

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,106 +1,92 @@
-import 'dart:developer';
-
-import 'package:dio/dio.dart';
-
-import 'todo.dart';
+import 'dart:convert';
+import 'package:flutter_todo_app/todo.dart';
+import 'package:http/http.dart' as http;
 
 class ApiService {
-  final Dio _dio = Dio();
-  var baseUrl = "https://mobileproject2.000webhostapp.com/";
+  final String baseUrl = "https://mobileproject2.000webhostapp.com/";
+
   Future<List<ToDo>> fetchToDos() async {
     try {
-      final response = await _dio.get(
-          "${baseUrl}get_todo.php"); // Replace with your actual API endpoint
+      final response = await http.get(Uri.parse("${baseUrl}get_todo.php"));
 
       if (response.statusCode == 200) {
-        final List<dynamic> todoListJson = response.data['todos'];
+        final List<dynamic> todoListJson = json.decode(response.body)['todos'];
         return todoListJson.map((json) => ToDo.fromJson(json)).toList();
       } else {
-        throw Exception('Failed to fetch todos');
+        print('Failed to fetch todos - ${response.statusCode}');
+        return []; // Return an empty list or handle the error accordingly
       }
     } catch (e) {
-      log(e.toString());
-      throw Exception('Failed to fetch todos: $e');
+      print('Error fetching todos: $e');
+      return []; // Return an empty list or handle the error accordingly
     }
   }
 
   Future<ToDo> addNewToDo(String todoText) async {
     var headers = {'Content-Type': 'application/x-www-form-urlencoded'};
     var data = {'todoText': todoText, 'isDone': 'false'};
+
     try {
-      var response = await _dio.request(
-        "${baseUrl}add_todo.php",
-        options: Options(
-          method: 'POST',
-          headers: headers,
-        ),
-        data: data,
+      var response = await http.post(
+        Uri.parse("${baseUrl}add_todo.php"),
+        headers: headers,
+        body: data,
       );
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> todoJson = response.data['todo'];
+        final Map<String, dynamic> todoJson = json.decode(response.body)['todo'];
         return ToDo.fromJson(todoJson);
       } else {
         throw Exception('Failed to add todo');
       }
     } catch (e) {
+      print('Error adding todo: $e');
       throw Exception('Failed to add todo: $e');
     }
   }
 
-  Future<ToDo> updateToDoStatus(
-      String todoText, String todoId, bool isDone) async {
+  Future<ToDo> updateToDoStatus(String todoText, String todoId, bool isDone) async {
     var headers = {'Content-Type': 'application/x-www-form-urlencoded'};
-    var data = {
-      'todoText': todoText,
-      'isDone': isDone,
-      'id': todoId,
-    };
+    var data = {'todoText': todoText, 'isDone': isDone.toString(), 'id': todoId};
+
     try {
-      var response = await _dio.request(
-        "${baseUrl}update_todo.php",
-        options: Options(
-          method: 'POST',
-          headers: headers,
-        ),
-        data: data,
+      var response = await http.post(
+        Uri.parse("${baseUrl}update_todo.php"),
+        headers: headers,
+        body: data,
       );
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> todoJson = response.data['todo'];
+        final Map<String, dynamic> todoJson = json.decode(response.body)['todo'];
         return ToDo.fromJson(todoJson);
       } else {
         throw Exception('Failed to edit todo');
       }
     } catch (e) {
-      log(e.toString());
+      print(e.toString());
       throw Exception('Failed to edit todo: $e');
     }
   }
 
   Future<ToDo> deleteToDo(String todoId) async {
     var headers = {'Content-Type': 'application/x-www-form-urlencoded'};
-    var data = {
-      'id': todoId,
-    };
+    var data = {'id': todoId};
+
     try {
-      var response = await _dio.request(
-        "${baseUrl}delete_todo.php",
-        options: Options(
-          method: 'POST',
-          headers: headers,
-        ),
-        data: data,
+      var response = await http.post(
+        Uri.parse("${baseUrl}delete_todo.php"),
+        headers: headers,
+        body: data,
       );
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> todoJson = response.data['todo'];
+        final Map<String, dynamic> todoJson = json.decode(response.body)['todo'];
         return ToDo.fromJson(todoJson);
       } else {
-        throw Exception('Failed to add todo');
+        throw Exception('Failed to delete todo');
       }
     } catch (e) {
-      throw Exception('Failed to add todo: $e');
+      throw Exception('Failed to delete todo: $e');
     }
   }
 }
